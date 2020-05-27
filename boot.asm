ff84209a0682399f2263d2bbb97d968410908e7d
Denis <cailean393@gmail.com>
2020-05-27 09:26:09 +0300

    add org directive

--- boot.asm
+++ boot.asm
@@ -3,39 +3,13 @@
   ; int 0x10 		- screen-related interrupts
   ; ah <- 0x0e  	- bios scrolling teletype routine   
   ; al <- ascii char	- char to display
+  
+  [org 0x7c00]
 
   mov ah, 0x0e		; enable teletype mode
 
-  mov al, 'h'
-  int 0x10
-  mov al, 'e'
-  int 0x10
-  mov al, 'l'
-  int 0x10
-  mov al, 'l'
-  int 0x10
-  mov al, 'o'
-  int 0x10
-  mov al, ' '
-  int 0x10
-  mov al, 't'
-  int 0x10
-  mov al, ' '
-  int 0x10
-
   ; secret guess
-  mov al, secret
-  int 0x10
-
   mov al, [secret]
-  int 0x10
-
-  mov bx, secret
-  add bx, 0x7c00
-  mov al, [bx]
-  int 0x10 
- 
-  mov al, [0x7c1e]
   int 0x10
 
   jmp $			; $ - current address, endless loop
