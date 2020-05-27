ca741213e7e3e45c7b43603ff2378759d61dc14d
Denis <cailean393@gmail.com>
2020-05-27 09:50:40 +0300

    simple stack

--- boot.asm
+++ boot.asm
@@ -8,14 +8,34 @@
 
   mov ah, 0x0e		; enable teletype mode
 
-  ; secret guess
-  mov al, [secret]
+  ; simple stack
+
+  mov bp, 0x8000
+  mov sp, bp
+
+  push 'A'
+  push 'B'
+  push 'C'
+
+  ; print C
+  pop bx
+  mov al, bl
+  int 0x10
+
+  ; print B
+  pop bx
+  mov al, bl
+  int 0x10
+
+  ; print A
+  pop bx
+  mov al, bl
   int 0x10
 
   jmp $			; $ - current address, endless loop
 
   secret:
-    db "X"
+    db "My awesome OS!", 0
 
   times 510-($-$$) db 0	 ; padding, fill 510 zeros
   dw 0xaa55		 ; boot sector signature 
