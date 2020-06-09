a92215eda689af1567e11edb701bd96911c60c20
Denis <cailean393@gmail.com>
2020-06-10 00:59:28 +0300

    switch to protection mode

--- bios_helpers/read_second_sector.asm
+++ bios_helpers/read_second_sector.asm
@@ -1,3 +1,6 @@
+  xor ax, ax
+  mov ds, ax
+  cld
   mov ah, 0x02 ; bios read sector
   mov dl, [BOOT_DRIVE] ; 0x00, 0x01 - floppy; 0x80, 0x81 - hard disk
   mov ch, 0 ; cylinder
@@ -6,9 +9,9 @@
   mov al, 1 ; read n sectors
 
   ; set segment which we want to read to [ES:BX]
-  mov bx, 0x7e00 ; segment - right after 1st boot sector
+  mov bx, 0x0 ; segment - right after 1st boot sector
   mov es, bx
-  mov bx, 0x0 ; offset
+  mov bx, 0x7e00 ; offset
 
   int 0x13 ; interrupt for disk routines
 
--- boot.asm
+++ boot.asm
@@ -1,14 +1,14 @@
 ; boot sector
 
- ;[org 0x7c00]
+  [org 0x7c00]
   [bits 16]
 
   ; prepare stack frame
   mov bp, 0x7c00    ; 30kb memory available down to 0x500 (0x500-0x7c00)
   mov sp, bp
 
-  mov bx, 0x7c0
-  mov ds, bx        ; set data segment 
+  ; mov bx, 0x7c0
+  ; mov ds, bx        ; set data segment 
 
   ; save boot drive
   mov [BOOT_DRIVE], dl
@@ -28,7 +28,7 @@
   
   disk_success:
     ; jump to 2nd sector, absolute address 
-    jmp 0x7e00:0x00
+    jmp 0x00:0x7e00
 
   end:
 
@@ -37,7 +37,7 @@
   disk_error_msg: db 'Cannot read disk sector', 0
   BOOT_DRIVE: db 0
 
-  %include "sum_func.asm"
+  ; %include "sum_func.asm"
   %include "bios_helpers/print_hex_address.asm"
   %include "bios_helpers/print_bin_address.asm"
   %include "bios_helpers/print_dec_address.asm"
@@ -51,26 +51,10 @@
   dw 0xaa55		             ; boot sector signature 
   
   ; ----------------------
-  ; SECTOR 2
+  ; SECTOR 2 - 0x7e00
   ; ----------------------
 
-  VIDEO_MEMORY equ 0xb8000
-  WHITE_ON_BLACK equ 0x0f
-
-  ; mov bx, 0xb800 ; start of video memory
-  ; mov es, bx
-  ; mov bx, 0x00 ; offset
-
-  ; ; write 'X' on screen
-  ; mov al, 'X'
-  ; mov ah, WHITE_ON_BLACK
-  ; mov [es:bx], ax
-
-  ; ;mov ebx, hello_msg
-  ; ;call print_string
-
-
-  [bits 32]  
+  jmp SWITCH_TO_PROTECTION_MODE
 
   GDT_START:
     GDT_NULL:   ; null descriptor (8 bytes)
@@ -102,13 +86,53 @@
     dw GDT_END - GDT_START - 1
     dd GDT_START
 
-
   ; some useful constant for segment registers
   ; when we set DS = 0x10, thats mean offset from GDT
   CODE_SEG equ GDT_CODE - GDT_START
   DATA_SEG equ GDT_DATA - GDT_START
+
+  SWITCH_TO_PROTECTION_MODE:
+
+    cli   ; disable interrupts
+    lgdt [GDT_DESCRIPTOR]   ; load gdt table
+
+    ; set 1st bit of cr0 to switch mode
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    
+    jmp CODE_SEG:INIT_PROTECTION_MODE
+
   
-  jmp $
+  [bits 32]
+
+  INIT_PROTECTION_MODE:
+    ; update segment registers
+    ; data segments
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; code segment
+    ; mov ax, CODE_SEG
+    ; mov cs, ax
+
+    ; update stack
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    ; sti   ; enable interrupts
+
+    mov ebx, hello_msg
+    call print_string_x
+    jmp $
+
+  
+  VIDEO_MEMORY equ 0xb8000
+  WHITE_ON_BLACK equ 0x0f
 
   ; ebx - pointer to string
   print_string_x:
@@ -128,7 +152,7 @@
       ret
 
 
-  hello_msg: db 'Hello there'
+  hello_msg: db 'Hello from protection mode', 0
 
   times 512 db 'V'
   times 512 db 'M'
