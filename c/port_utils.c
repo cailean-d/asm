bb27a2b7775fb71a47fc27da630612017a219650
Denis <cailean393@gmail.com>
2020-06-12 05:32:12 +0300

    simple asm editing

--- c/port_utils.c
+++ c/port_utils.c
@@ -2,20 +2,20 @@
 
 unsigned char port_byte_read(unsigned short port) {
   unsigned char result;
-  __asm__("in %% dx, %% al" : "=a" (result) : "d" (port));
+  __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
   return result;
 }
 
 void port_byte_write(unsigned short port, unsigned char data) {
-  __asm__("out %% al, %% dx" : : "a" ( data ), "d" (port));
+  __asm__("out %%al, %%dx" : : "a" ( data ), "d" (port));
 }
 
 unsigned short port_word_read(unsigned short port) {
   unsigned short result;
-  __asm__("in %% dx, %% ax" : "=a" (result) : "d" (port));
+  __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
   return result;
 }
 
 void port_word_write(unsigned short port, unsigned short data) {
-  __asm__("out %% ax, %% dx" : : "a" (data), "d" (port));
+  __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
